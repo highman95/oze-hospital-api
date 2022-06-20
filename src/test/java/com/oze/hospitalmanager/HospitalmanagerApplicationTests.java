960ba6a887cf219fb38e4430109ae4d14bd7a77b
Emma NWAMAIFE <emadimabua@gmail.com>
2022-06-20 15:02:27 +0100

    test: update test-cases for staff endpoints

--- src/test/java/com/oze/hospitalmanager/HospitalmanagerApplicationTests.java
+++ src/test/java/com/oze/hospitalmanager/HospitalmanagerApplicationTests.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.oze.hospitalmanager.controllers.StaffController;
 
 @SpringBootTest
+@DirtiesContext
 class HospitalmanagerApplicationTests {
 
 	@Autowired
--- src/test/java/com/oze/hospitalmanager/integration/StaffControllerTest.java
+++ src/test/java/com/oze/hospitalmanager/integration/StaffControllerTest.java
@@ -3,8 +3,10 @@ package com.oze.hospitalmanager.integration;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,10 +14,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,52 +41,91 @@ import com.oze.hospitalmanager.utils.Constants;
 @DirtiesContext
 public class StaffControllerTest {
 
-        @InjectMocks
-        private StaffService staffService;
+    @Spy
+    private StaffService staffService;
 
-        @Mock
-        private IStaffRepository staffRepository;
+    @Mock
+    private IStaffRepository staffRepository;
 
-        @Autowired
-        private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-        @Autowired
-        private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        @Test
-        @DisplayName("should return 400 when creating a staff with no-name")
-        void whenNoNameIsPassed_ThenReturnStatus400() throws Exception {
-                String body = objectMapper.writeValueAsString(new StaffDto());
+    @Test
+    @DisplayName("should return 400 when creating a staff with no-name")
+    void whenNoNameIsPassed_ThenReturnStatus400() throws Exception {
+        String body = objectMapper.writeValueAsString(new StaffDto());
 
-                mockMvc.perform(post("/api/v1/staffs")
-                                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
-                                .andExpect(status().isBadRequest())
-                                .andExpect(jsonPath("$.status", is(false)))
-                                .andExpect(jsonPath("$.data", is(nullValue())))
-                                .andExpect(jsonPath("$.message", is(Constants.NAME_IS_REQUIRED)));
-        }
+        mockMvc.perform(post("/api/v1/staffs")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status", is(false)))
+                .andExpect(jsonPath("$.data", is(nullValue())))
+                .andExpect(jsonPath("$.message", is(Constants.NAME_IS_REQUIRED)));
+    }
 
-        @Test
-        @DisplayName("should return 201 when creating a staff with valid name")
-        void whenValidNameIsPassed_ThenReturnStatus201() throws Exception {
-                StaffDto staffDto = new StaffDto("John Wick");
-                String body = objectMapper.writeValueAsString(staffDto);
+    @Test
+    @DisplayName("should return 201 when creating a staff with valid name")
+    void whenValidNameIsPassed_ThenReturnStatus201() throws Exception {
+        StaffDto staffDto = new StaffDto("John Wick");
+        String body = objectMapper.writeValueAsString(staffDto);
 
-                Staff staff = Staff.builder().name(staffDto.getName())
-                                .uuid(UUID.randomUUID()).registrationDate(LocalDate.now())
-                                .build();
+        Staff staff = Staff.builder().name(staffDto.getName())
+                .uuid(UUID.randomUUID()).registrationDate(LocalDate.now())
+                .build();
 
-                when(staffService.addProfile(staffDto))
-                                .thenReturn(new Response<>(true, staff, Constants.DATA_SUCCESSFULLY_SAVED));
+        when(staffService.addProfile(staffDto))
+                .thenReturn(new Response<>(true, staff, Constants.DATA_SUCCESSFULLY_SAVED));
 
-                mockMvc.perform(post("/api/v1/staffs")
-                                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
-                                .andExpect(status().isCreated())
-                                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                                .andExpect(jsonPath("$.status", is(true)))
-                                .andExpect(jsonPath("$.data", is(notNullValue())))
-                                .andExpect(jsonPath("$.data.name", is(staff.getName())))
-                                .andExpect(jsonPath("$.message", is(Constants.DATA_SUCCESSFULLY_SAVED)));
-        }
+        mockMvc.perform(post("/api/v1/staffs")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status", is(true)))
+                .andExpect(jsonPath("$.data", is(notNullValue())))
+                .andExpect(jsonPath("$.data.name", is(staff.getName())))
+                .andExpect(jsonPath("$.message", is(Constants.DATA_SUCCESSFULLY_SAVED)));
+    }
+
+    @Test
+    @DisplayName("should return 403 when updating staff-profile with valid name and no-auth-token")
+    void whenNoAuthTokenIsPassed_ThenReturnStatus403() throws Exception {
+        StaffDto staffDto = new StaffDto("James Wick");
+        String body = objectMapper.writeValueAsString(staffDto);
+
+        mockMvc.perform(put("/api/v1/staffs")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("should return 403 when updating staff-profile with valid name and invalid uuid (for authentication)")
+    void whenInvalidAuthTokenIsPassed_ThenReturnStatus403() throws Exception {
+        StaffDto staffDto = new StaffDto("James Wick");
+        String body = objectMapper.writeValueAsString(staffDto);
+
+        mockMvc.perform(put("/api/v1/staffs")
+                .header(Constants.CUSTOM_AUTH_TOKEN_NAME, "1234567890abcdefghijk")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Disabled("the testcase is under development")
+    @DisplayName("should return 204 when updating staff-profile with valid name and uuid (for authentication)")
+    void whenValidAuthTokenIsPassed_ThenReturnStatus204() throws Exception {
+        StaffDto staffDto = new StaffDto("James Wick");
+        String body = objectMapper.writeValueAsString(staffDto);
+
+        Staff staff = Staff.builder().uuid(UUID.randomUUID()).registrationDate(LocalDate.now()).build();
+        doNothing().when(staffService).updateProfile(staff.getUuid(), staffDto);
+
+        mockMvc.perform(put("/api/v1/staffs")
+                .header(Constants.CUSTOM_AUTH_TOKEN_NAME, staff.getUuid())
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(status().isNoContent());
+    }
 
 }
